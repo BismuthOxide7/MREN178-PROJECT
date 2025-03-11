35ac27129a6c521ffde68dcf5de1240676857281
erinpayne <erin.payne.01@gmail.com>
2025-03-10 21:21:57 -0400

    Update display.h

--- display.h
+++ display.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 #include "cards.h"
 
-//function prototypes 
+// Function prototypes 
 int add_player_card(Card_struct &card); // Display draws card from the deck
 int add_hidden_dealer_card(Card_struct &card); // Displayed dealers hidden card
 int add_visable_dealer_card(Card_struct &card); // Displayed dealer card
