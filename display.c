996b4615cb516574dc664a26d341632d30dc996a
erinpayne <erin.payne.01@gmail.com>
2025-03-10 22:57:48 -0400

    Update display.c

--- display.c
+++ display.c
@@ -18,18 +18,30 @@ struct Node createNode(struct Card_struct card){
   return newNode;
 }
 
+// Update LCD 
+void update_LCD(){
+  // TODO: Update display with linked lists and hidden cards 
+}
+
 // Function implimentations
 int add_player_card(struct Card_struct &card){
+  // TODO: Add card to player cards linked list 
+  update_LCD(); 
   return 0;
 }
 
 int add_hidden_dealer_card(struct Card_struct &card){
   hidden_dealer_card = card;
+  update_LCD(); 
   return 0;
 }
 int add_visable_dealer_card(struct Card_struct &card){
+  // TODO: Add card to dealer visable cards linked list 
+  update_LCD();
   return 0;
 }
 int new_game (){
+  // TODO: delete all pre-existing cards in linked lists and hidden dealer card
+  update_LCD(); 
   return 0;
 }
