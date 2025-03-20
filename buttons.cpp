df3edd545aa008e9e93c4f81efaedee0d0dc3ef3
YasminHammadi <ymhammadi9@gmail.com>
2025-03-20 15:02:40 -0400

    Update buttons.cpp

--- buttons.cpp
+++ buttons.cpp
@@ -1,41 +1,15 @@
-#include "buttons.h"
+#ifndef BUTTONS_H
+#define BUTTONS_H
 
-void setupButtons() {
-    pinMode(BTN_HIT, INPUT_PULLUP);
-    pinMode(BTN_STAY, INPUT_PULLUP);
-    pinMode(BTN_FOLD, INPUT_PULLUP);
-    pinMode(BTN_BET, INPUT_PULLUP);
-    pinMode(BTN_VIEW, INPUT_PULLUP);
-}
+#include <Arduino.h>
+#include <LiquidCrystal.h> 
 
-void checkButtons() {
-    if (digitalRead(BTN_HIT) == LOW) {
-        Serial.println("Hit button pressed!");
-        hc12_send(CMD_HIT);
-        delay(300);
-    }
+// LCD Keypad Shield uses A0 for button input
+#define BTN_PIN A0  
 
-    if (digitalRead(BTN_STAY) == LOW) {
-        Serial.println("Stay button pressed!");
-        hc12_send(CMD_STAY);
-        delay(300);
-    }
+// Menu navigation
+void setupButtons();
+void checkButtons();
+void updateMenu();
 
-    if (digitalRead(BTN_FOLD) == LOW) {
-        Serial.println("Fold button pressed!");
-        hc12_send(CMD_FOLD);
-        delay(300);
-    }
-
-    if (digitalRead(BTN_BET) == LOW) {
-        Serial.println("Bet button pressed!");
-        hc12_send(CMD_BET);
-        delay(300);
-    }
-
-    if (digitalRead(BTN_VIEW) == LOW) {
-        Serial.println("View Other Hand button pressed!");
-        hc12_send(CMD_RECEIVE_THIS_CARD);
-        delay(300);
-    }
-}
+#endif
