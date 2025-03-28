a36a90a6dcd07527b7194ed190eb3e22ec06ecca
Ben Dunkley <109436549+BismuthOxide7@users.noreply.github.com>
2025-03-28 13:36:38 -0400

    Update dealer.h
    cleaned up

--- main_sketch/dealer.h
+++ main_sketch/dealer.h
@@ -7,22 +7,5 @@
 #include "display.h"
 #include "actions.h"
 
-typedef enum {
-    GAME_WAITING,
-    GAME_DEALING,
-    GAME_PLAYING,
-    GAME_ENDING
-} GameState;
-
-// External declarations
-extern GameState gameState;
-extern player* players[4];  // Use the player struct from actions.h
-extern uint8_t currentPlayer;
-
-// Function prototypes
-void dealer_init(void);
-void dealer_background_task(void);
-void dealer_handle_command(CommandPacket packet);
-void start_new_game(void);
 
 #endif
