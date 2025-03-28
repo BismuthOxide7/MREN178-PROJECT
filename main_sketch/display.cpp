d7b294ba7b47d9c076202f617a7057c6ff434966
Ben Dunkley <109436549+BismuthOxide7@users.noreply.github.com>
2025-03-28 13:37:38 -0400

    Update display.cpp
    REVERT BACK

--- main_sketch/display.cpp
+++ main_sketch/display.cpp
@@ -6,99 +6,123 @@
 #include <LiquidCrystal.h>
 #include "display.h"
 #include <Arduino.h>
+#include <Arduino.h>
+#include "cards.h"
+#include "display.h"
+#include <LiquidCrystal.h>
 
-// Initialize the library by associating any needed LCD interface pin
-// with the Arduino pin number it is connected to
-const int pin_RS = 8;
-const int pin_EN = 9;
-const int pin_d4 = 4;
-const int pin_d5 = 5;
-const int pin_d6 = 6;
-const int pin_d7 = 7;
-const int pin_BL = 10;
-LiquidCrystal lcd(pin_RS, pin_EN, pin_d4, pin_d5, pin_d6, pin_d7);  // Properly named object
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to      
+const int pin_RS = 8; 
+const int pin_EN = 9; 
+const int pin_d4 = 4; 
+const int pin_d5 = 5; 
+const int pin_d6 = 6; 
+const int pin_d7 = 7; 
+const int pin_BL = 10; 
+LiquidCrystal lcd(pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
 
-// Define global variables
-Card_struct hidden_dealer_card;
-Node* player_cards = NULL;
-Node* dealer_cards = NULL;
+// Define global variables 
+struct Card_struct hidden_dealer_card;
+struct Card_struct* player_cards = NULL;
+struct Card_struct* dealer_cards = NULL;
+
+// Struct to define linked list for added player cards and added visable dealer cards with two different heads 
+struct Node {
+  struct Node* next;
+  struct Card_struct card;
+};
 
 // Create node
-Node* createNode(Card_struct card) {
-  Node* newNode = (Node*)malloc(sizeof(Node));
+struct Node createNode(struct Card_struct card){
+  struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
   newNode->card = card;
-  newNode->next = NULL;
   return newNode;
 }
 
-// Update LCD display with linked lists and hidden card
-void update_LCD() {
+// Update LCD display with linked lists and hidden card 
+void update_LCD(){
   lcd.clear();
-  lcd.setCursor(0, 0);
-  Node* transverser = player_cards;
-  while (transverser != NULL) {
-    lcd.print(transverser->card.friendlyName);
-    transverser = transverser->next;
+  lcd.setCursor(0,0);
+  struct Node* transverser = player_cards;
+  while(transverser != NULL){
+    lcd.print(transverser -> card.friendlyName);
+    transverser = transverser.next;
   }
-  lcd.setCursor(1, 0);
-  lcd.print("H");  // Printing hidden card - later needs to be shown
-  lcd.setCursor(1, 1);
+  lcd.setCursor(1,0);
+  // TODO; need to replace with if statement with a flag to check if the hidden card needs to be flipped over 
+  lcd.print ("H"); //printing hidden card - later needs to be shown 
+  lcd.setCursor(1,1);
   transverser = dealer_cards;
-  while (transverser != NULL) {
-    lcd.print(transverser->card.friendlyName);
-    transverser = transverser->next;
-  }
+  while(transverser != NULL){
+    lcd.print(transverser -> card.friendlyName);
+    transverser = transverser.next;
+  } 
 }
 
-// Function implementations for player cards linked list
-int add_player_card(Card_struct* card) {
-  Node* node = createNode(*card);
-  if (player_cards == NULL) {
-    player_cards = node;
-  } else {
-    node->next = player_cards;
-    player_cards = node;
+// Function implimentations player cards linked list 
+int add_player_card(struct Card_struct &card){
+  struct node = createNode(card); 
+  if (player_cards == NULL){
+    player_cards = &node;
   }
-  update_LCD();
+  else {
+    node.next = play_cards;
+    player_cards = &node;
+  }
+  update_LCD(); 
   return 0;
 }
 
-// Function implementations for hidden dealer card
-int add_hidden_dealer_card(Card_struct* card) {
-  hidden_dealer_card = *card;
-  update_LCD();
+//Function implimentations hidden dealer card
+int add_hidden_dealer_card(struct Card_struct &card){
+  hidden_dealer_card = card;
+  update_LCD(); 
   return 0;
+  
 }
-
-// Function implementations for dealer visible cards linked list
-int add_visible_dealer_card(Card_struct* card) {
-  Node* node = createNode(*card);
-  if (dealer_cards == NULL) {
-    dealer_cards = node;
-  } else {
-    node->next = dealer_cards;
-    dealer_cards = node;
+//Function implimentations dealer visable cards linked list 
+int add_visable_dealer_card(struct Card_struct &card){
+  struct node = createNode(card); 
+  if (dealer_cards == NULL){
+    dealer_cards = &node;
   }
-  update_LCD();
+  else {
+    node.next = dealer_cards;
+    dealer_cards = &node;
+  }
+  update_LCD(); 
   return 0;
 }
 
-// Clear linked list
-int clear_list(Node* p_head) {
-  Node* p_temp;
+int clear_list(struct Node* p_head){
+  Node *p_temp;
+  Node *p_tail;
   while (p_head != NULL) {
-    p_temp = p_head;
-    p_head = p_head->next;
-    free(p_temp);
-  }
-  return 0;
+      p_temp = p_head;
+      // Checking if there is only one node in the list and if so deleting it 
+      if(p_temp -> next == NULL){
+        p_head = NULL;
+        free(p_temp);
+      }
+      // Search for the node preceeding the tail node. Once found, date the old tail node, and update this preceeding node to become 
+      // The new tail node by setting p_next_node to null and setting p_tail to point to this node 
+      else{
+        while (p_temp -> next -> next != NULL){
+          p_temp=p_temp->next;
+        }
+        p_tail = p_temp -> next;
+        p_temp -> next = NULL;
+        free(p_tail); 
+      }
+    }
 }
-
-// Delete all pre-existing cards in linked lists and hidden dealer card
-int new_game() {
-  clear_list(player_cards);
-  clear_list(dealer_cards);
-  hidden_dealer_card = {0};  // Reset hidden dealer card
-  update_LCD();
+// delete all pre-existing cards in linked lists and hidden dealer card
+int new_game (){
+  clear_list(player_list);
+  clear_list(dealer_list);
+  hidden_dealer_card = NULL;
+  
+  update_LCD(); 
   return 0;
 }
