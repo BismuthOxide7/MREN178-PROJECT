195e35ed0d292b4ab0d6917ef1c7a2816e6d5a2a
Ashleigh-Cunningham <cunninghama557@gmail.com>
2025-03-30 21:25:24 -0400

    Update actions.h

--- main_sketch/actions.h
+++ main_sketch/actions.h
@@ -10,6 +10,7 @@ extern typedef struct player {
     int totalSum = 0;          
     *Card_struct head;
     *Card_struct next;
+    bool over21 = false; // if the player goes above 21
     bool stand = false;
     bool fold = false;
     int totalBet = 0;
